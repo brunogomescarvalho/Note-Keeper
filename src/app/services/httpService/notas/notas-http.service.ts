7fdc0b1a08887c0e0793546e7e8079cbf5ba50a0
Bruno G Carvalho <113460204+brunogomescarvalho@users.noreply.github.com>
2023-09-20 17:49:24 -0300

    add environment

--- src/app/services/httpService/notas/notas-http.service.ts
+++ src/app/services/httpService/notas/notas-http.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Nota } from 'src/app/models/nota';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
